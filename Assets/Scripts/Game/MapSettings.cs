5313ab7a790e9e239fc7e8fc11efae1f7fe94dc1
Hochoy M <51301524+mhochoy@users.noreply.github.com>
2025-10-15 20:06:24 -0400

    Added Beach Map Preview
    - More visible 'enable/disable damage' text
    - Removed redundant properties from MapSettings asset

--- Assets/Scripts/Game/MapSettings.cs
+++ Assets/Scripts/Game/MapSettings.cs
@@ -6,10 +6,7 @@ using System.Collections.Generic;
 public class MapSettings : ScriptableObject
 {
     public string mapName;
-    [Tooltip("Where the car should be waiting on the map picker screen")]
     public Transform PlayerSpawnTransform;
-    public List<Transform> BotSpawnTransforms = new List<Transform>();
-    public Vector3 WaitingTransform;
     public int TotalRacers;
     public bool Traps;
     public GameObject Environment;
